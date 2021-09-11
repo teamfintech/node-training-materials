e52f924262bd66a5313c032f8c970271a767bbf4
ovichowdhury <ovi.chowdhury@hotmail.com>
2021-09-11 19:43:56 +0600

    insertion sort corrected

--- algorithms/sorting/insertion-sort.js
+++ algorithms/sorting/insertion-sort.js
@@ -1,27 +1,29 @@
 
-
-function insertionSort(arr, order = "ASC") {
+/**
+ * Worst Case Time Complexity O(n^2)
+ * Algorithm
+ * 1. Divide the array into two half sorted and unsorted (initially 0, 1..n)
+ * 2. for each unsorted element make a room in the sorted array
+ * 3. Insert the key in the room after each interation
+ */
+function insertionSort(arr, order= "ASC") {
     for (let j = 1; j < arr.length; j++) {
         const key = arr[j];
         let i = j - 1;
-        while(i >= 0) {
-            switch(order) {
-                case "ASC":
-                    if(key < arr[i]) {
-                        arr[i+1] = arr[i];
-                        arr[i] = key;
-                    }
-                    break;
-                case "DESC":
-                    if(key > arr[i]) {
-                        arr[i+1] = arr[i];
-                        arr[i] = key;
-                    }
-                    break;
+        if(order === "ASC") {
+            while(i >= 0 && key < arr[i]) {
+                arr[i+1] = arr[i];
+                i--;
             }
-            
-            i--;
         }
+        else {
+            while(i >= 0 && key > arr[i]) {
+                arr[i+1] = arr[i];
+                i--;
+            }
+        }
+        
+        arr[i+1] = key;
     }
     return arr;
 }
