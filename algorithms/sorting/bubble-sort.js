d8784601e6fd35939410fbd7932b428b869a94ae
ovichowdhury <ovi.chowdhury@hotmail.com>
2021-09-14 23:01:09 +0600

    bubble sort added

---
+++ algorithms/sorting/bubble-sort.js
@@ -0,0 +1,16 @@
+
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < (arr.length - i) - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+module.exports = bubbleSort;
